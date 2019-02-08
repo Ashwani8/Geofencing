3c4a50b338da808cc7929249b0299ccfbe53ddb1
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-02-08 17:08:45 -0600

    get the list of Geofence event

--- app/src/main/java/com/example/geofencing/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/example/geofencing/GeofenceTransitionsIntentService.java
@@ -1,16 +1,22 @@
 package com.example.geofencing;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.Nullable;
 
 public class GeofenceTransitionsIntentService extends IntentService {
     private static final String TAG = "gfservice";
+    private Context context;
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
      *
@@ -47,6 +53,50 @@ public class GeofenceTransitionsIntentService extends IntentService {
             geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT){
             // Get the geofences that were triggered. A single event can trigger
             // multiple geofences.
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            // 4c Get the transition details as a String.
+            // Get the transition details as a String.
+            String geofenceTransitionDetails = getGeofenceTransitionDetails(
+                    this,
+                    geofenceTransition,
+                    triggeringGeofences
+            );
+            // Send notification and log the transition details. step 5 create sendNotification helper method
+           // this is where I need to add media code
+
+            Log.i(TAG, geofenceTransitionDetails);
+        }else {
+            // Log the error.
+            Log.e(TAG, getString(R.string.geofence_transition_invalid_type,
+                    geofenceTransition));
         }
+    }
+    // 4d build the method to
+
+    /**
+     * Gets transition details and returns them as a formatted string.
+     * @param context context
+     * @param geofenceTransition The ID of the geofence transition.
+     * @param triggeringGeofences The geofence(s) triggered.
+     * @return  The transition details formatted as String.
+     */
+    private String getGeofenceTransitionDetails(
+            Context context,
+            int geofenceTransition,
+            List<Geofence> triggeringGeofences) {
+        String geofenceTransitionString = getTransitionString(geofenceTransition);
+
+        // Get the Ids of each geofence that was triggered.
+        ArrayList<String> triggeringGeofencesIdsList = new ArrayList<>();
+        for (Geofence geofence : triggeringGeofences) {
+            triggeringGeofencesIdsList.add(geofence.getRequestId());
+        }
+        String triggeringGeofencesIdsString = TextUtils.join(", ",  triggeringGeofencesIdsList);
+
+        return geofenceTransitionString + ": " + triggeringGeofencesIdsString;
+    }
+
+    private String getTransitionString(int geofenceTransition) {
     }
 }
