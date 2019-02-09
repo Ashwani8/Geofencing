8638580ad4a3ae1007412c23e8e17ad890b9c6d4
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-02-08 22:49:56 -0600

    Populate Geofence list

--- app/src/main/java/com/example/geofencing/MainActivity.java
+++ app/src/main/java/com/example/geofencing/MainActivity.java
@@ -12,8 +12,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -21,15 +26,60 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean mLocationPermissionGranted =  false;
 
+    /**
+     * Provides access to the Geofencing API.
+     */
     private GeofencingClient mGeofencingClient;
+
+    /**
+     * The list of geofences used in this project.
+     */
+    private ArrayList<Geofence> mGeofenceList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Empty list for storing geofences.
+        mGeofenceList = new ArrayList<>();
+        // Get the geofences used. Geofence data is hard coded in this project.
+        populateGeofenceList();
         mGeofencingClient = LocationServices.getGeofencingClient(this);
     }
 
+    /**
+     * This sample hard codes geofence data. A real app might dynamically create geofences based on
+     * the user's location.
+     */
+    private void populateGeofenceList() {
+        for (Map.Entry<String, LatLng> entry : Constants.BAY_AREA_LANDMARKS.entrySet()) {
+
+            mGeofenceList.add(new Geofence.Builder()
+                    // Set the request ID of the geofence. This is a string to identify this
+                    // geofence.
+                    .setRequestId(entry.getKey())
+
+                    // Set the circular region of this geofence.
+                    .setCircularRegion(
+                            entry.getValue().latitude,
+                            entry.getValue().longitude,
+                            Constants.GEOFENCE_RADIUS_IN_METERS
+                    )
+
+                    // Set the expiration duration of the geofence. This geofence gets automatically
+                    // removed after this period of time.
+                    .setExpirationDuration(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+
+                    // Set the transition types of interest. Alerts are only generated for these
+                    // transition. We track entry and exit transitions in this sample.
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                            Geofence.GEOFENCE_TRANSITION_EXIT)
+
+                    // Create the geofence.
+                    .build());
+        }
+    }
 
 
     public void addGeofencesButtonHandler(View view) {
